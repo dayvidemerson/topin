b5d302bbd2cd39d8202e2a354847415a3d66325e
Dayvid Emerson <dayvidemerson@rease.com.br>
2018-02-23 23:50:14 -0300

    Corrigindo bug de filtro de horários

--- apps/route/filters.py
+++ apps/route/filters.py
@@ -13,19 +13,19 @@ class MarkerFilter(django_filters.FilterSet):
 
 class LineFilter(django_filters.FilterSet):
     city = django_filters.CharFilter(name='city__slug')
-    weekdays = django_filters.CharFilter(name='weekdays__in')
 
     class Meta:
         model = Line
-        fields = ['city', 'weekdays']
+        fields = ['city']
 
 
 class ScheduleFilter(django_filters.FilterSet):
     line = django_filters.CharFilter(name='line__slug')
+    weekdays = django_filters.CharFilter(name='weekdays__in')
 
     class Meta:
         model = Schedule
-        fields = ['line']
+        fields = ['line', 'weekdays']
 
 
 class RouteFilter(django_filters.FilterSet):
